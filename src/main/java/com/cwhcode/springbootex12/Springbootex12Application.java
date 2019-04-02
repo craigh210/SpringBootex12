7a364abf4da4b5bcf4dcf9af3644aa594e542ca7
craigh210 <craighunt210@gmail.com>
2019-04-02 17:17:26 -0400

    Initial commit

---
+++ src/main/java/com/cwhcode/springbootex12/Springbootex12Application.java
@@ -0,0 +1,13 @@
+package com.cwhcode.springbootex12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootex12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootex12Application.class, args);
+    }
+
+}
